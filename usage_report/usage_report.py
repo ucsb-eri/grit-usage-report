071a8b664611d79c5945d36f19bddc600f78dfc1
Tjaart van der Walt <git@tjaart.org>
2026-01-30 14:13:47 -0800

    Fix column error

--- usage_report/usage_report.py
+++ usage_report/usage_report.py
@@ -109,8 +109,8 @@ def run():
                 main_sheet.write(idx + 1, 4, properties.get("grit:owner"))
                 # main_sheet.write(idx + 1, 5, properties.get("grit:projectcode"))
                 # main_sheet.write(idx + 1, 6, properties.get("grit:lafscode"))
-                main_sheet.write(idx + 1, 7, properties.get("grit:ccoa"))
-                main_sheet.write_datetime(idx + 1, 8, timestamp.replace(tzinfo=None), date)
+                main_sheet.write(idx + 1, 5, properties.get("grit:ccoa"))
+                main_sheet.write_datetime(idx + 1, 6, timestamp.replace(tzinfo=None), date)
 
                 if size_terrabytes == 0:
                     main_sheet.set_row(idx + 1, None, None, {"hidden": True})
